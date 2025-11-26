e7c75912f929db22b539409964049407d830ed6a
ProgramerPlus <hipitelo@outlook.com>
2025-11-26 20:57:16 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+string chatbot="--- Groot Chatbot ---";
+Console.WriteLine(chatbot);
+
+string pergunta="";
+int n=0;
+
+while(pergunta!="TCHAU")
+{
+string groot="Eu sou Groot.";
+
+if(n==0){
+Console.Write("Digite sua pergunta ao Groot: ");
+
+}else{
+Console.WriteLine("Pergunta: ");
+}
+
+pergunta=Console.ReadLine()!;
+pergunta=pergunta.ToUpper();
+
+if(pergunta!="TCHAU"){
+     Console.WriteLine($"Resposta: {groot}");
+
+     if(n==0){ 
+    Thread.Sleep(2000);
+    Console.Clear();
+    n=1;
+    Console.WriteLine(chatbot);
+}
+
+
+}
+else
+{groot="Eu sou Groot!";
+Console.WriteLine($"Resposta: {groot}");
+n=1;
+}
+
+}
+
